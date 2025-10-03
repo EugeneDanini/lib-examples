d0cbbd3cc690e12efa736f0988fe5b681d235825
ED <e.danini@gmail.com>
2025-10-03 16:01:55 +0200

    update adk example

--- lib_google_adk/example.py
+++ lib_google_adk/example.py
@@ -1,67 +1,24 @@
-import datetime
-from zoneinfo import ZoneInfo
 from google.adk.agents import Agent
 
-def get_weather(city: str) -> dict:
-    """Retrieves the current weather report for a specified city.
 
-    Args:
-        city (str): The name of the city for which to retrieve the weather report.
-
-    Returns:
-        dict: status and result or error msg.
-    """
-    if city.lower() == "new york":
-        return {
-            "status": "success",
-            "report": (
-                "The weather in New York is sunny with a temperature of 25 degrees"
-                " Celsius (77 degrees Fahrenheit)."
-            ),
-        }
-    else:
-        return {
-            "status": "error",
-            "error_message": f"Weather information for '{city}' is not available.",
-        }
-
-
-def get_current_time(city: str) -> dict:
-    """Returns the current time in a specified city.
-
-    Args:
-        city (str): The name of the city for which to retrieve the current time.
-
-    Returns:
-        dict: status and result or error msg.
-    """
-
-    if city.lower() == "new york":
-        tz_identifier = "America/New_York"
-    else:
-        return {
-            "status": "error",
-            "error_message": (
-                f"Sorry, I don't have timezone information for {city}."
-            ),
-        }
-
-    tz = ZoneInfo(tz_identifier)
-    now = datetime.datetime.now(tz)
-    report = (
-        f'The current time in {city} is {now.strftime("%Y-%m-%d %H:%M:%S %Z%z")}'
-    )
-    return {"status": "success", "report": report}
+def get_hued(text: str) -> dict:
+    text = text.split(' ')
+    for i, word in enumerate(text):
+        if len(word) > 3:
+            word = f'hui{word[3:].lower()}'
+        text[i] = word
+    text = ' '.join(text)
+    return {"status": "success", "report": text}
 
 
 root_agent = Agent(
-    name="weather_time_agent",
+    name="hui_agent",
     model="gemini-2.0-flash",
     description=(
-        "Agent to answer questions about the time and weather in a city."
+        "Agent to huificate"
     ),
     instruction=(
-        "You are a helpful agent who can answer user questions about the time and weather in a city."
+        "You are a helpful agent who can huificate."
     ),
-    tools=[get_weather, get_current_time],
+    tools=[get_hued],
 )
